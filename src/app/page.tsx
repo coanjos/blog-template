134d7a7a68d26da611f03a2d825894c8db0bbcb5
Caio dos Anjos <coa.anjos@gmail.com>
2025-01-17 22:21:33 -0300

    adiciona shadcn

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,4 +1,11 @@
+import { Button } from '@/components/ui/button';
+
 function Home() {
-  return <div>Home</div>;
+  return (
+    <div className='flex items-center justify-center h-screen bg-gray-100'>
+      <h1 className='text-3xl font-bold text-gray-800'>Hello, World!</h1>
+      <Button>Botão Teste</Button>
+    </div>
+  );
 }
 export default Home;

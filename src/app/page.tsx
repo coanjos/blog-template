bd996d05d5b011954a6a11542e604a286dab583c
Caio dos Anjos <coa.anjos@gmail.com>
2025-01-18 17:52:43 -0300

    Adiciona nextui

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,3 +1,10 @@
 export default function Home() {
-  return <h1>TESTE</h1>;
+  return (
+    <div className='flex flex-col items-center mx-auto w-full max-w-lg  p-6 rounded-lg'>
+      <h1 className='text-5xl font-bold text-gray-800 mb-4'>Blog Exemplo</h1>
+      <p className='text-gray-600 text-center'>
+        Este é um exemplo do layout do blog.
+      </p>
+    </div>
+  );
 }

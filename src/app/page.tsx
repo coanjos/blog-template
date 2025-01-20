05af29f94d8cb9125ea13c7f86b1fbe6d68c1b5a
Caio dos Anjos <coa.anjos@gmail.com>
2025-01-20 17:44:42 -0300

    Cria um componente de footer

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,6 +1,6 @@
 export default function Home() {
   return (
-    <div className='flex flex-col items-center mx-auto w-full max-w-lg  p-6 rounded-lg'>
+    <div className='flex flex-col min-h-[calc(100vh-128px)] items-center mx-auto w-full max-w-lg  p-6 rounded-lg'>
       <h1 className='text-5xl font-bold text-gray-800 mb-4'>Blog Exemplo</h1>
       <p className='text-gray-600 text-center'>
         Este é um exemplo do layout do blog.
